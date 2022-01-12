0365c54f415655a20b26731da5498b96f50e8e2e
Stanisław Griszko <s.griszko@gmail.com>
2022-01-13 00:39:42 +0100

    add bg change

--- js/script.js
+++ js/script.js
@@ -44,22 +44,22 @@ const carsDB = [
 // const cars = document.querySelector('.cars');
 
 //show 3 cards with car name, country and so on
-// function showcarDB(cars) {
-//   const element = document.createElement('div');
-//   cars.forEach((car) => {
-//     element.classList.add('car-card');
-//     element.innerHTML += `
-//         <div class="car-card__item">
-//             <span>${car.car}</span>
-//             <span>${car.setup[0].country}</span>
-//             <span>${car.setup[0].conditions}</span>
-//             <span>${car.setup[0].tyres}</span>
-//         </div>
-//     `;
-//   });
-//   document.querySelector('.cars').append(element);
-// }
-// showcarDB(carsDB);
+function showcarDB(cars) {
+  const element = document.createElement('div');
+  cars.forEach((car) => {
+    element.classList.add('car-card');
+    element.innerHTML += `
+        <div class="car-card__item">
+            <span>${car.car}</span>
+            <span>${car.setup[0].country}</span>
+            <span>${car.setup[0].conditions}</span>
+            <span>${car.setup[0].tyres}</span>
+        </div>
+    `;
+  });
+  document.querySelector('.cars').append(element);
+}
+showcarDB(carsDB);
 
 //clicking on car card
 // const carCards = document.querySelectorAll('.car-card__item');
@@ -78,12 +78,24 @@ enterBtn.addEventListener('click', () => {
   loadSelectionPage();
 });
 
-function loadSelectionPage() {
+function addHide() {
   enterScreen.classList.add('hide');
+}
 
+function removeHide() {
   cars.classList.remove('hide');
   country.classList.remove('hide');
+}
 
+function changeBG() {
+  document.querySelector('main').style.backgroundImage =
+    'url(../img/selectionPage-bg.jpg)';
+}
+
+function loadSelectionPage() {
+  addHide();
+  removeHide();
   cars.classList.add('show');
   country.classList.add('show');
+  changeBG();
 }
