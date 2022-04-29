81424588fe96b49e6169a49fa5c116ea029d1abd
Stanisław Griszko <s.griszko@gmail.com>
2022-04-29 22:40:15 +0200

    css layout v1

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   let tree = document.querySelector('.wrapper');
-  let test = document.querySelector('.test');
+  let setupsContainer = document.querySelector('.setups-container');
   const url = 'db.json';
   let carCards;
   let setupsParent;
   let setupCountry;
   let s;
+  let backBtn;
 
   //create cards with cars and setups
   async function createCarCard() {
@@ -19,16 +20,18 @@ window.addEventListener('DOMContentLoaded', () => {
         const carCard = document.createElement('div');
         carCard.classList.add('car-card');
         carCard.innerHTML = `
-        <div class='car-card__image'>
-            <img src="${car.img}" alt="">
-            <div class="car-card__title"><span>${car.carName}</span></div>
-        </div>
-        <div class="car-card__setups setups">
-        </div>
+          <div class='car-card__image'>
+              <img src="${car.img}" alt="">
+              <div class="car-card__title"><span>${car.carName}</span></div>
+              <div class="button">
+                <button class="button__back">Close</button>
+              </div>
+          </div>
+          <div class="car-card__countries"></div>
         `;
         tree.append(carCard);
         ///
-        let carCardSetups = document.querySelectorAll('.car-card__setups');
+        let carCardSetups = document.querySelectorAll('.car-card__countries');
 
         ///
         // car.setup.forEach((setup) => {
@@ -51,91 +54,99 @@ window.addEventListener('DOMContentLoaded', () => {
 
         ///
         let stp = document.createElement('div');
-        stp.classList.add('setup');
-        test.appendChild(stp);
+        stp.classList.add('setups');
+        setupsContainer.appendChild(stp);
         ///
         car.setup.forEach((setup) => {
           const e = document.createElement('div');
-          e.classList.add('setup-country__item');
+          e.classList.add('setups__country');
           e.innerHTML = `
-          <div class="item__tyres">
-              <span>Tyres</span>
-              <span>${setup.tyres}</span>
-          </div>
-          
-          <div class="item__conditions">
-              <span>Conditions</span>
-              <span>${setup.conditions}</span>
-          </div>
+          <div class="country">
+            <div classs="country-title"><span>Country: ${setup.country}</span></div>
 
-          <div class="item__alignment">
-              <span>Alignment</span> 
-              <div>Toe Angle Front: ${setup.alignment.toeAngleFront} °</div>
-              <div>Camber Angle Front: ${setup.alignment.camberAngleFront} °</div>
-              <div>Toe Angle Rear: ${setup.alignment.toeAngleRear} °</div>
-              <div>Camber Angle Rear: ${setup.alignment.camberAngleRear} °</div>
+            <div class="item__tyres">
+                <span>Tyres</span>
+                <span>${setup.tyres}</span>
+            </div>
+
+            <div class="item__conditions">
+                <span>Conditions</span>
+                <span>${setup.conditions}</span>
+            </div>
+
           </div>
+          <div class="setup__item">
+            <div class="item__alignment">
+                <div>Alignment</div>
+                <div>Toe Angle Front: ${setup.alignment.toeAngleFront} °</div>
+                <div>Camber Angle Front: ${setup.alignment.camberAngleFront} °</div>
+                <div>Toe Angle Rear: ${setup.alignment.toeAngleRear} °</div>
+                <div>Camber Angle Rear: ${setup.alignment.camberAngleRear} °</div>
+            </div>
 
-          <div class="item__brakes">
-              <span>Brakes</span> 
-              <div>Brake Bias: ${setup.brakes.brakingForce} N-m</div>
-              <div>Braking force: ${setup.brakes.brakeBias} %</div>
-          </div>
+            <div class="item__brakes">
+                <div>Brakes</div> 
+                <div>Brake Bias: ${setup.brakes.brakingForce} N-m</div>
+                <div>Braking force: ${setup.brakes.brakeBias} %</div>
+            </div>
 
-          <div class="item__dif">
-              <span>Differential</span>
-              <div>Front LSD Driving lock: ${setup.differential.frontLSDDrivingLock} %</div>
-              <div>Front LSD Braking Lock: ${setup.differential.frontLSDBrakingLock} %</div>
-              <div>Front LSD Preload: ${setup.differential.frontLSDPreload} N-m</div>
-              <div>Front Viscous Differential: ${setup.differential.frontVisDif} kgf-m/100 rpm</div>
-              <div>Centre Viscous Differential: ${setup.differential.centerVisDif} kgf-m/100 rpm</div>
-              <div>Rear Viscous Differential: ${setup.differential.rearVisDif} kgf-m/100 rpm</div>
-          </div>
+            <div class="item__dif">
+                <div>Differential</div>
+                <div>Front LSD Driving lock: ${setup.differential.frontLSDDrivingLock} %</div>
+                <div>Front LSD Braking Lock: ${setup.differential.frontLSDBrakingLock} %</div>
+                <div>Front LSD Preload: ${setup.differential.frontLSDPreload} N-m</div>
+                <div>Front Viscous Differential: ${setup.differential.frontVisDif} kgf-m/100 rpm</div>
+                <div>Centre Viscous Differential: ${setup.differential.centerVisDif} kgf-m/100 rpm</div>
+                <div>Rear Viscous Differential: ${setup.differential.rearVisDif} kgf-m/100 rpm</div>
+            </div>
 
-          <div class="item__gear">
-              <span>Gearing</span>
-              <div>1st Gear: ${setup.gearing.gear1}</div>
-              <div>2nt Gear: ${setup.gearing.gear2}</div>
-              <div>3rd Gear: ${setup.gearing.gear3}</div>
-              <div>4th Gear: ${setup.gearing.gear4}</div>
-              <div>4th Gear: ${setup.gearing.gear5}</div>
-              <div>4th Gear: ${setup.gearing.gear6}</div>
-              <div>Final Drive: ${setup.gearing.finalDrive}</div>
-              <div>Note: ${setup.gearing.note}</div>
-              <div>Optimal Shift: ${setup.gearing.optimalShift}</div>
-          </div>
+            <div class="item__gear">
+                <div>Gearing</div>
+                <div>1st Gear: ${setup.gearing.gear1}</div>
+                <div>2nt Gear: ${setup.gearing.gear2}</div>
+                <div>3rd Gear: ${setup.gearing.gear3}</div>
+                <div>4th Gear: ${setup.gearing.gear4}</div>
+                <div>4th Gear: ${setup.gearing.gear5}</div>
+                <div>4th Gear: ${setup.gearing.gear6}</div>
+                <div>Final Drive: ${setup.gearing.finalDrive}</div>
+                <div>Note: ${setup.gearing.note}</div>
+                <div>Optimal Shift: ${setup.gearing.optimalShift}</div>
+            </div>
 
-          <div class="item__damp">
-              <span>Damping</span>
-              <div>Slow Bump Front: ${setup.damping.slowBumpFront}</div>
-              <div>Fast Bump Front: ${setup.damping.fastBumpFront}</div>
-              <div>Bump Zone Division Front: ${setup.damping.bumpZoneDivFront}</div>
-              <div>Slow Rebound Front: ${setup.damping.slowReboundFront}</div>
+            <div class="item__damp">
+                <div>Damping</div>
+                <div>Slow Bump Front: ${setup.damping.slowBumpFront}</div>
+                <div>Fast Bump Front: ${setup.damping.fastBumpFront}</div>
+                <div>Bump Zone Division Front: ${setup.damping.bumpZoneDivFront}</div>
+                <div>Slow Rebound Front: ${setup.damping.slowReboundFront}</div>
 
-              <div>Slow Bump Rear: ${setup.damping.slowBumpRear}</div>
-              <div>Fast Bump Rear: ${setup.damping.fastBumpRear}</div>
-              <div>Bump Zone Division Rear: ${setup.damping.bumpZoneDivRear}</div>
-              <div>Slow Rebound FroRearnt: ${setup.damping.slowReboundRear}</div>
-          </div>
+                <div>Slow Bump Rear: ${setup.damping.slowBumpRear}</div>
+                <div>Fast Bump Rear: ${setup.damping.fastBumpRear}</div>
+                <div>Bump Zone Division Rear: ${setup.damping.bumpZoneDivRear}</div>
+                <div>Slow Rebound FroRearnt: ${setup.damping.slowReboundRear}</div>
+            </div>
 
-          <div class="item__springs">
-              <span>Springs</span>
-              <div>Ride Height Front: ${setup.springs.rideHeightFront} mm</div>
-              <div>Spring Rate Front: ${setup.springs.springRateFront} N/mm</div>
-              <div>Ride Height Rear: ${setup.springs.rideHeightRear} mm</div>
-              <div>Spring Rate Rear: ${setup.springs.springRateRear} N/mm</div>
+            <div class="item__springs">
+                <div>Springs</div>
+                <div>Ride Height Front: ${setup.springs.rideHeightFront} mm</div>
+                <div>Spring Rate Front: ${setup.springs.springRateFront} N/mm</div>
+                <div>Ride Height Rear: ${setup.springs.rideHeightRear} mm</div>
+                <div>Spring Rate Rear: ${setup.springs.springRateRear} N/mm</div>
+            </div>
           </div>
           `;
           stp.appendChild(e);
         });
 
         carCards = document.querySelectorAll('.car-card');
-        setupsParent = document.querySelectorAll('.setups');
+        setupsParent = document.querySelectorAll('.car-card__countries');
         setupCountry = document.querySelectorAll('.setup-country');
-        s = document.querySelectorAll('.setup-country__item');
-        console.log(s);
-        console.log(setupCountry);
+        s = document.querySelectorAll('.setups__country');
+        backBtn = document.querySelectorAll('.button');
+        // console.log(s);
+        // console.log(setupCountry);
 
+        y();
         getSetups(setupsParent, setupCountry);
       });
       // console.log(setupsParent);
@@ -144,6 +155,15 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
   createCarCard();
+
+  function y() {
+    backBtn.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        closeCarCard();
+        btn.classList.remove('show');
+      });
+    });
+  }
 
   // const carCards = document.querySelectorAll('.car-card'), // not working
   const cardsParent = document.querySelector('.wrapper');
@@ -155,6 +175,7 @@ window.addEventListener('DOMContentLoaded', () => {
       // console.log(target);
       if (target.tagName == 'IMG') {
         carCards.forEach((item, i) => {
+          console.log(carCards);
           if (target.parentElement.parentNode == item) {
             // console.log(item);
             closeCarCard();
@@ -204,12 +225,13 @@ window.addEventListener('DOMContentLoaded', () => {
         card.classList.add('hide');
       }
     });
+    backBtn[item].classList.add('show');
   }
 
   // remove class 'selected' and 'setup__item--active'
   function closeSetups() {
     setupCountry.forEach((item) => {
-      item.classList.remove('selected');
+      item.classList.remove('chosen');
     });
     s.forEach((item) => {
       item.classList.remove('setup-country__item--active');
@@ -218,7 +240,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // add class 'selected' and class 'setup__item--active'
   function openSetups(item) {
-    setupCountry[item].classList.add('selected');
+    setupCountry[item].classList.add('chosen');
     s[item].classList.add('setup-country__item--active');
   }
 
@@ -254,4 +276,5 @@ window.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.main').style.backgroundImage =
       'url(../img/selectionPage-bg.jpg)';
   }
+  //
 });
